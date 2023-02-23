115a0c990210c8725c490da9ae5a1d38f98ff5aa
cwaina <chrismnairobi@gmail.com>
2023-02-23 10:56:10 -0500

    budget app

--- src/components/CartValue.js
+++ src/components/CartValue.js
@@ -9,7 +9,7 @@ const CartValue = () => {
 
     return (
 <div className='alert alert-primary'>
-    <span>Cart Value: {Location}{totalExpenses}</span>
+    <span>Budget: {Location}{totalExpenses}</span>
     </div>
     );
 };
